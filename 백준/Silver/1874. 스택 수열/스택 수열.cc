cdf894b468b560188374353186a95a2f7e2dd2fd
강건우 <64396933+eric1306@users.noreply.github.com>
2024-11-30 23:53:38 +0900

    [Silver II] Title: 스택 수열, Time: 12 ms, Memory: 2896 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,44 +1,27 @@
 #include <iostream>
 #include <stack>
-#include <vector>
-
+#include <string>
 using namespace std;
-
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	int n,k,i;
-	int j=1;
-	stack<int> s;
-	vector<char> vec;
-	cin>>n;
-	for(i=1;i<=n;i++){
-		cin>>k;
-		if(s.empty()){
-			s.push(j++);
-			vec.push_back('+');
-		}
-		if(k==s.top()){
-			vec.push_back('-');
-			s.pop();
-		}else{
-			for(;j<=k;j++){
-				vec.push_back('+');
-				s.push(j);
-			}
-			if(k==s.top()){
-				vec.push_back('-');
-				s.pop();
-			}else{
-				cout<<"NO"<<'\n';
-				break;
-			}
-		}
-	}
-	if(i==n+1){
-		for(int l=0;l<vec.size();l++)
-			cout<<vec.at(l)<<'\n';
-	}
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,k,cnt = 1;
+    string ans;
+    cin>>n;
+    stack<int> s;
+    while(n--){
+        cin>>k;
+        while(cnt <=k){
+            s.push(cnt++);
+            ans+="+\n";
+        }
+        if(s.top()!=k){
+            cout<<"NO";
+            return 0;
+        }
+        s.pop();
+        ans+="-\n";
+        
+    }
+    cout<<ans;
 }
