9dca5c0c59b8e4dc00088c94b82f878d36980388
강건우 <64396933+eric1306@users.noreply.github.com>
2024-12-08 19:36:27 +0900

    [Gold V] Title: 토마토, Time: 72 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,55 +1,49 @@
-#include <iostream>
-#include <utility>
-#include <queue>
-#include <string>
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : -
+// http://boj.kr/ae38aa7eb7a44aca87e9d7928402d040
+#include <bits/stdc++.h>
 using namespace std;
-#define x first
-#define y second
-
+#define X first
+#define Y second
 int board[1002][1002];
+int dist[1002][1002];
 int n,m;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int day = 0;
-    cin>>n>>m;
-    queue<pair<int,int>> q;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cin>>board[i][j];
-        }
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> m >> n;
+  queue<pair<int,int> > Q;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      cin >> board[i][j];
+      if(board[i][j] == 1)
+        Q.push({i,j});
+      if(board[i][j] == 0)
+        dist[i][j] = -1;
     }
-
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(board[i][j] == 1) q.push({i,j});
-        }
+  }
+  while(!Q.empty()){
+    auto cur = Q.front(); Q.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if(dist[nx][ny] >= 0) continue;
+      dist[nx][ny] = dist[cur.X][cur.Y]+1;
+      Q.push({nx,ny});
     }
-    //하루가 지나는 것을 파악하는 방법?
-    while(!q.empty()){
-        pair<int,int> p = q.front();
-        q.pop();
-        for(int i=0;i<4;i++){
-            int nx = p.x + dx[i];
-            int ny = p.y + dy[i];
-            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if(board[nx][ny] != 0) continue;
-            q.push({nx,ny});
-            board[nx][ny] = board[p.x][p.y]+1;
-        }
+  }
+  int ans = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(dist[i][j] == -1){
+        cout << -1;
+        return 0;
+      }
+      ans = max(ans, dist[i][j]);
     }
-
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(board[i][j] == 0) {
-                cout<<-1;
-                return 0;
-            }
-            if(day < board[i][j]) day = board[i][j];
-        }
-    }
-    cout<<day-1;
+  }
+  cout << ans;
 }
