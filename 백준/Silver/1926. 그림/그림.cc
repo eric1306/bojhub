b2238a1066d2b89eb5733a26456c24d1a3c22eca
강건우 <64396933+eric1306@users.noreply.github.com>
2024-12-08 18:47:13 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,54 +1,52 @@
 #include <iostream>
 #include <utility>
 #include <queue>
-
-#define X first
-#define Y second
+using namespace std;
+#define x first
+#define y second
 
 int board[502][502];
 bool visit[502][502];
-
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-using namespace std;
+int n,m;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 int main(){
-    int n,m,num = 0,maxSize = 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int ans = 0;
+    int max = 0;
     cin>>n>>m;
-
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cin>>board[i][j];
         }
     }
+    for(int i=0;i<n;i++) fill(visit[i],visit[i]+m,false);
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            queue<pair<int,int>> q;
-            int qsize = 0;
-
-            if(board[i][j] == 1 && !visit[i][j]){
-                visit[i][j] = 1;
+            if(!visit[i][j] && board[i][j] == 1){
+                int qsize = 0;
+                queue<pair<int,int>> q;
                 q.push({i,j});
-                qsize++;
-
+                visit[i][j] = 1;
                 while(!q.empty()){
                     pair<int,int> p = q.front();
+                    qsize++;
                     q.pop();
                     for(int k=0;k<4;k++){
-                        int nx = p.X + dx[k];
-                        int ny = p.Y + dy[k];
-                        if(nx <0 || nx >= n || ny <0 || ny >= m) continue;
-                        if(visit[nx][ny] || board[nx][ny] != 1) continue;
+                        int nx = p.x + dx[k];
+                        int ny = p.y + dy[k];
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                        if(visit[nx][ny] || board[nx][ny]!=1) continue;
                         visit[nx][ny] = 1;
                         q.push({nx,ny});
-                        qsize++;
                     }
                 }
-                num++;
-                if(maxSize < qsize) maxSize = qsize;
+                ans++;
+                if(max < qsize) max = qsize;
             }
         }
     }
-    cout<<num<<'\n'<<maxSize;
+    cout<<ans<<'\n'<<max;
 }
