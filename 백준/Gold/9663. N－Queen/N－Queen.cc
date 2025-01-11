84853b9e0be50eda7841caefd8fd7415141fa499
강건우 <64396933+eric1306@users.noreply.github.com>
2025-01-11 19:15:49 +0900

    [Gold IV] Title: N-Queen, Time: 1804 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,52 +3,25 @@ using namespace std;
 
 int n,ans;
 int arr[16][16];
-
-bool IsSpot(int x, int y){
-    for(int i=0;i<n;i++){
-        if(i == x) continue;
-
-        if(arr[i][y]) 
-            return false;
-    }
-    int xPos = x;
-    int yPos = y;
-    while(--xPos >= 0 && --yPos >= 0){
-        if(arr[xPos][yPos]) return false;
-    }
-    xPos = x;
-    yPos = y;
-    while(--xPos >= 0 && ++yPos < n){
-        if(arr[xPos][yPos]) return false;
-    }
-    xPos = x;
-    yPos = y;
-    while(++xPos < n && --yPos >= 0){
-        if(arr[xPos][yPos]) return false;
-    }
-    xPos = x;
-    yPos = y;
-    while(++xPos < n && ++yPos < n){
-        if(arr[xPos][yPos]) return false;
-    }
-
-    return true;
-}
+int isused1[30];
+int isused2[30];
+int isused3[30];
 
 void func(int k){
     if(k == n){
         ans++;
         return;
     }
-    
-    for(int j=0;j<n;j++){
-        if(!arr[k][j] && IsSpot(k,j))
-        {
-            //cout<<i<<j<<' '<<k+1<<'\n';
-            arr[k][j] = 1;
-            func(k+1);
-            arr[k][j] = 0;
-        }
+    for (int i = 0; i < n; i++) {
+        if(isused1[i] || isused2[i+k] || isused3[k-i+n-1])
+            continue;
+        isused1[i] = 1;
+        isused2[i+k] = 1;
+        isused3[k-i+n-1] = 1;
+        func(k+1);
+        isused1[i] = 0;
+        isused2[i+k] = 0;
+        isused3[k-i+n-1] = 0;
     }
 }
 
