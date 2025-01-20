c8f73db7c54141e415a3f6cba9232b4418d55d9b
강건우 <64396933+eric1306@users.noreply.github.com>
2025-01-21 07:39:11 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -1,55 +1,43 @@
-#include <iostream>
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : -
+// http://boj.kr/550e5f94d9504613971281134eebaeae
+#include <bits/stdc++.h>
 using namespace std;
-
 int n;
-int egg[10][2];
-int break_egg;
+int s[10],w[10];
+int mx = 0;
+int cnt = 0; // 깨져있는 계란의 수
 
-void func(int k){
-    if(k == n) /*가장 오른쪽 계란 든 경우 계란 치기 종료*/
-    {
-        int tmp = 0;
-        for(int i=0;i<n;i++){
-            if(egg[i][0] <=0) tmp++;
-        }
-        if(tmp > break_egg) break_egg = tmp;
-        return;
-    }
-
-    for(int i = 0;i<n;++i)/*k번째 계란으로 부숴지지 않은 계란 부수기 시도*/
-    {
-        if(k == i) continue;
-        if(egg[k][0] <= 0)
-        {
-            func(k+1);
-            return;
-        }
-
-        if(egg[i][0] > 0)
-        {
-            egg[k][0] -= egg[i][1];
-            egg[i][0] -= egg[k][1];
-            func(k+1);
-            egg[k][0] += egg[i][1];
-            egg[i][0] += egg[k][1];
-        }
-        else if(egg[i][0] <= 0)
-        {
-            func(k+1);
-        }
-        
-    }
+void solve(int a){ // a번째 계란으로 다른걸 깰 차례
+  if(a == n){
+    mx = max(mx,cnt);
+    return;
+  }
+  if(s[a] <= 0 or cnt == n-1){
+    solve(a+1);
+    return;
+  } // a번째 계란이 깨져있거나 다른 모든 계란이 깨져있으면 넘어감
+  for(int t = 0; t < n; t++){ // t번째 계란을 내려치고 싶다
+    if(t == a or s[t] <= 0) continue;
+    s[a] -= w[t];
+    s[t] -= w[a];
+    if(s[a] <= 0) cnt++;
+    if(s[t] <= 0) cnt++;
+    solve(a+1);
+    if(s[a] <= 0) cnt--;
+    if(s[t] <= 0) cnt--;    
+    s[a] += w[t];
+    s[t] += w[a];
+  }
 }
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>egg[i][0]>>egg[i][1]; //내구도, 무게
-    }
-    /*k번째 계란으로 부수기*/
-    func(0);
-    
-    cout<<break_egg;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> s[i] >> w[i];
+  }
+  solve(0);
+  cout << mx;  
 }
