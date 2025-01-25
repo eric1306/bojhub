872bb5f553cd247d477b608166c58529b36fb0fd
강건우 <64396933+eric1306@users.noreply.github.com>
2025-01-25 17:13:18 +0900

    [Gold IV] Title: Puyo Puyo, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.cc
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 #define ROW 12
 #define COL 6
+
 #define x first
 #define y second
 
@@ -16,45 +17,9 @@ int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 queue<pair<int,int>> deleteq;
 
-void print_puyo(){
-    for(int i=0;i<ROW;i++){
-        for(int j=0;j<COL;j++){
-            cout<<board[i][j]<<' ';
-        }
-        cout<<'\n';
-    }
-}
 bool OOB(int nx, int ny){
     return nx  < 0 || nx >= 12 || ny < 0 || ny >=6;
 }
-// bool erasePuyo(int x, int y){
-//     queue<pair<int,int>> q;
-//     queue<pair<int,int>> tmp;
-//     q.push({x,y});
-//     tmp.push({x,y});
-//     visit[x][y] = true;
-//     while(!q.empty()){
-//         pair<int,int> p = q.front();
-//         q.pop();
-//         for(int i=0;i<4;i++){
-//             int nx = p.x + dx[i];
-//             int ny = p.y + dy[i];
-//             if(OOB(nx,ny) || visit[nx][ny] || board[nx][ny]!=board[p.x][p.y]) continue;
-//             visit[nx][ny] = 1;
-//             q.push({nx,ny});
-//             tmp.push({nx,ny});
-//         }
-//     }
-//     //cout<<"tmp size: "<<tmp.size()<<'\n';
-//     if(tmp.size() >= 4){
-//         while(!tmp.empty()){
-//             board[tmp.front().x][tmp.front().y] = '.';
-//             tmp.pop();
-//         }
-//         return true;
-//     }
-//     return false;
-// }
 
 bool check(){
     /*R, Y, G*/
@@ -78,7 +43,6 @@ bool check(){
                         visit[nx][ny] = 1;
                         tmp.push({nx,ny});
                         q.push({nx,ny});
-                        //cout<<nx<<','<<ny<<'\n';
                     }
                 }
                 if(tmp.size() >= 4){
@@ -120,18 +84,6 @@ int main(){
     
     bool flag = false;
     while(check()){
-        //cout<<"while start"<<'\n';
-        // flag = false;
-        // for(int i=0;i<ROW;i++) fill(visit[i],visit[i]+6,false);
-        // for(int i=0;i<ROW;i++){
-        //     for(int j=0;j<COL;j++){
-        //         if(!visit[i][j] && board[i][j]!='.'){
-        //             if(erasePuyo(i,j)){
-        //                 flag = true;
-        //             }
-        //         }
-        //     }
-        // }
         while(!deleteq.empty()){
             pair<int,int> p = deleteq.front();
             deleteq.pop();
