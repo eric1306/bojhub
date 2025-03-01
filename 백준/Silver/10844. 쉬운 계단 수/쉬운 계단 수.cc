88f400ffe5ee9901d86c9b9c28b2dbca0de3f107
강건우 <64396933+eric1306@users.noreply.github.com>
2025-03-02 08:57:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,39 +1,33 @@
+//Authored by: prid1306
 #include <iostream>
+#include <algorithm>
+#include <string>
+
+#define MODENUM 1'000'000'000
 using namespace std;
 
-long long D[101][101];
-const long long mod=1000000000;
-int limit=100;
+int n;
+int d[101][10];
 
-void ansf(){
-	for(int j=1;j<=9;j++){ //여기는 인정 
-		D[1][j]=1;
-	}
-	D[1][0]=0;
-	for(int i=2;i<=limit;i++){
-		for(int j=0;j<=9;j++){
-			if(j-1>=0&&j+1<10){//1~8 사이
-				D[i][j]=D[i-1][j-1]%mod+D[i-1][j+1]%mod;	
-			}else if(j==0){
-				D[i][j]=D[i-1][j+1]%mod;
-			}else if(j==9){
-				D[i][j]=D[i-1][j-1]%mod;
-			}
-			D[i][j]%=mod;
-		}
-	}
-}
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int n;
-	long long ans=0;
-	cin>>n;
-	ansf();
-	for(int i=0;i<=9;i++){
-		ans+=D[n][i]%mod;
-	}
-	cout<<ans%mod;
-	
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=1;i<10;i++) d[1][i] = 1;
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            if(j!=0) d[i][j] += d[i-1][j-1];
+            if(j!=9) d[i][j] += d[i-1][j+1];
+            d[i][j]%=MODENUM;
+        }
+    }
+    
+    long long max = 0;
+    for(int i=0;i<10;i++){
+        max += d[n][i];
+    }
+    max%=MODENUM;
+    cout<<max;
 }
