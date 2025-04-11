b36253d92d55c1db6d44865f22adea28e9e68560
강건우 <64396933+eric1306@users.noreply.github.com>
2025-04-11 20:49:12 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -3,25 +3,19 @@
 #include <algorithm>
 using namespace std;
 int n,m;
-int a[100001];
+int a[100005];
+int mn = 0x7fffffff;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>m;
     for(int i=0;i<n;i++) cin>>a[i];
-    int st = 0,en = 0,ans = 0x7fffffff;
     sort(a,a+n);
-    while(st < n && en < n)
-    {
-        if(a[en] - a[st] < m)
-        {
-            en++;
-        }
-        else if(a[en] - a[st] >= m)
-        {
-            ans = min(ans, a[en]-a[st]);
-            st++;
-        }
+    int en = 0;
+    for(int st = 0;st < n;st++){
+        while(en < n && a[en] - a[st] < m) en++;
+        if(en == n) break;
+        mn = min(mn, a[en] - a[st]);
     }
-    cout<<ans;
+    cout<<mn;
 }
