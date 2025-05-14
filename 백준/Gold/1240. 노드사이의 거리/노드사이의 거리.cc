bd88be761aae3e36f811452c082331ed1f119b64
강건우 <64396933+eric1306@users.noreply.github.com>
2025-05-14 10:52:00 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -8,28 +8,25 @@ using namespace std;
 #define y second
 
 int n,m;
-int a[1001][1001];
+vector<pair<int,int>> v[1001];
 
 int findlen(int from, int to)
 {
-    vector<bool> visit(1001, false);
     queue<pair<int,int>> q;
-    q.emplace(from,0);
-    visit[from] = true;
+    vector<bool> vis(n+1);
+    q.emplace(from, 0);
+    vis[from] = true;
     while(!q.empty())
     {
-        pair<int,int> p = q.front();q.pop();
-        for(int i=1;i<=n;i++)
+        auto [cur,dist] = q.front();q.pop();
+        if(cur == to){
+            return dist;
+        }
+        for(auto [node, nodeDist] : v[cur])
         {
-            //연결짓는 가중치가 없거나 방문했었다면
-            if(p.x == i || a[p.x][i] == 0 || visit[i]) continue;
-            if(i == to){
-                //얘와의 거리를 반환하면 됨.
-                return a[p.x][i] + p.y;
-            }
-            //그게 아니면 queue에 넣기
-            q.emplace(i,p.y + a[p.x][i]);
-            visit[i] = true;
+            if(vis[node]) continue;
+            vis[node] = true;
+            q.emplace(node, dist + nodeDist);
         }
     }
     return 0;
@@ -43,12 +40,12 @@ int main(){
     for(int i=0;i<n-1;i++)
     {
         cin>>from>>to>>len;
-        a[from][to] = len;
-        a[to][from] = len;
+        v[from].emplace_back(to,len);
+        v[to].emplace_back(from,len);
     }
     while(m--)
     {
         cin>>from>>to;
-        cout<<findlen(from,to)<<'\n';
+        cout<<findlen(from, to)<<'\n';
     }
 }
