d764bda3ce17bf83b4a74572130037310e62b8f3
강건우 <64396933+eric1306@users.noreply.github.com>
2025-05-16 08:27:09 +0900

    [Gold IV] Title: 회사 문화 1, Time: 32 ms, Memory: 8316 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.cc
@@ -0,0 +1,45 @@
+//Authored by: prid1306
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int n,m;
+vector<int> parent(100'001, 0);
+vector<int> child[100'001];
+vector<int> ans(100'001, 0);
+
+void addpraise(int num, int praise)
+{
+    ans[num]+=praise;
+    for(auto c : child[num])
+    {
+        addpraise(c, praise);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        int input;
+        cin>>input;
+        child[input].push_back(i);
+        parent[i] = input;
+    }
+    
+    int num, praise;
+    while(m--)
+    {
+        cin>>num>>praise;
+        ans[num] += praise;
+    }
+    //타고 올라가면서 나의 부모가 칭찬 받으면 더해서 합산값 출력
+    cout<<0<<' ';
+    for(int i=2;i<=n;i++)
+    {
+        ans[i] += ans[parent[i]];
+        cout<<ans[i]<<' ';
+    }
+} 
