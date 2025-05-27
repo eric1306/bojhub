3457f9653f51af2e0bd6f7a0adac85c814ac6df0
강건우 <64396933+eric1306@users.noreply.github.com>
2025-05-27 10:22:58 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 376 ms, Memory: 20976 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,53 @@
+//Authored by: prid1306
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n,m,a,b,cost;
+
+vector<int> p(100002, -1);
+int find(int x)
+{
+    if(p[x] < 0) return x;
+    return p[x] = find(p[x]);
+}
+
+bool uni(int u, int v)
+{
+    u = find(u);v = find(v);
+    if(u == v) return false;
+    if(p[v] < p[u]) swap(u,v);
+    if(p[u] == p[v]) p[u]--;
+    p[v] = u;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    vector<tuple<int,int,int>> input;
+    for(int i=0;i<m;i++)
+    {
+        cin>>a>>b>>cost;
+        input.emplace_back(cost, a, b);
+    }
+    sort(input.begin(),input.end());
+    long ans = 0;
+    int maxValue = 0;
+    int cnt = 0;
+    for(auto v : input)
+    {
+        int from,to,cost;
+        tie(cost, from, to) = v;
+        if(uni(from,to))
+        {
+            ans+=cost;
+            cnt++;
+            if(maxValue < cost) maxValue = cost;
+        }
+        if(cnt == n-1) break;
+    }
+    cout<<ans - maxValue;
+}
