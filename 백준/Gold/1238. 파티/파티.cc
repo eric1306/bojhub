fd2810df9207bc4d7866d8f713f125ccea0eaab6
강건우 <64396933+eric1306@users.noreply.github.com>
2025-06-01 13:12:51 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,4 +1,4 @@
-//Authored by: prid1306
+//Authored by prid1306
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,44 +6,52 @@
 using namespace std;
 #define X first
 #define Y second
-int d[1001][1001];
 const int INF = 0x3f3f3f3f;
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n,m,x;
-    cin>>n>>m>>x;
-    vector<pair<int,int>> adj[n+1];
-    while(m--){
-        int a,b,cost;
-        cin>>a>>b>>cost;
-        adj[a].push_back({cost, b});
-    }
-    for(int i=1;i<=n;i++){
-        fill(d[i],d[i]+1+n,INF);
-    }
-    for(int i=1;i<=n;i++){
-        d[i][i] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({d[i][i],i});
-        while(!pq.empty()){
-            auto cur = pq.top();pq.pop();
-            if(d[i][cur.Y]!=cur.X) continue;
-            for(auto nxt : adj[cur.Y])
-            {
-                if(d[i][nxt.Y] <= d[i][cur.Y] + nxt.X) continue;
-                d[i][nxt.Y] = d[i][cur.Y] + nxt.X;
-                pq.push({d[i][nxt.Y], nxt.Y});
+vector<pair<int, int>> adj[1001];
+vector<pair<int, int>> adj_r[1001];
+
+vector<int> D(int st, const vector<pair<int, int>> adj[], int n) {
+    vector<int> dist(n + 1, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    dist[st] = 0;
+    pq.push({ 0, st });
+
+    while (!pq.empty()) {
+        auto cur = pq.top();pq.pop();
+        if (dist[cur.Y] != cur.X) continue;
+        for (auto nxt : adj[cur.Y]) {
+            if (dist[nxt.X] > dist[cur.Y] + nxt.Y) {
+                dist[nxt.X] = dist[cur.Y] + nxt.Y;
+                pq.push({ dist[nxt.X], nxt.X });
             }
         }
     }
-    //여기까지 O(ElgE)
-    //TODO: 왕복 거리 최대합 구하기
-    int ans = -1;
-    for(int i=1;i<=n;i++)
-    {
-        if(ans < d[i][x] + d[x][i]) ans = d[i][x] + d[x][i];
-    }
-    cout<<ans;
+    return dist;
+}
 
-} 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,m,x;
+    cin>>n>>m>>x;
+
+    while(m--) {
+        int a, b, cost;
+        cin >> a >> b >> cost;
+        adj[a].push_back({ b, cost });
+        adj_r[b].push_back({ a, cost }); 
+    }
+    vector<int> x_dist = D(x, adj, n);
+
+    vector<int> dist_x = D(x, adj_r, n);
+
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        int round_trip = dist_x[i] + x_dist[i];
+        answer = max(answer, round_trip);
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
