580732b676e79cf1e74038e12fe4281e522cd00a
강건우 <64396933+eric1306@users.noreply.github.com>
2025-07-05 14:01:06 +0900

    [Silver I] Title: 기타콘서트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.cc
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.cc
@@ -1,76 +1,45 @@
-//Authored by: prid1306
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n,m;
-long long state[11];
-int _min = 0x3fffffff;
-long long minsongs = 0;
-long long _max = 0;
-bool visit[11];
-void check(int count)
-{
-    if(count !=0 && count <= n)
-    {
-        long long ret = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(visit[i]) ret = ret | state[i];
-        }
-        if(ret == 0) return;
-        
-        if(ret > minsongs)
-        {
-            _min = count;
-            minsongs = ret;
-        }
-        else if(ret == minsongs)
-        {
-            if(_min > count)
-            {
-                _min = count;
-                minsongs = ret;
-            }
-        }
-        if(count == n) return;
-    }
+int n, m;
+long long state[10];
 
-    for(int i=0;i<n;i++)
-    {
-        if(!visit[i])
-        {
-            visit[i] = true;
-            check(count+1);
-            visit[i] = false;
-        }
-    }
+int bit_cnt(long long x){
+  int ret = 0;
+  for(int i = 0; i < max(n, m); i++){
+    ret += (x >> i) & 1;
+  }
+  return ret;
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>m;
-    for(int i=0;i<m;i++){
-        _max = _max<<1;
-        _max+=1;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    string name, tmp; // name은 사실 의미없음
+    cin >> name >> tmp;
+    for(int j = m-1; j >= 0; j--){
+      state[i] = (state[i] << 1) | (tmp[j] == 'Y');
     }
+  }
 
-    for(int i=0;i<n;i++)
-    {
-        string tmp, songs;
-        cin>>tmp>>songs;
-        for(int j=0;j<m;j++)
-        {
-            state[i] = state[i]<<1;
-            if(songs[j] == 'Y')
-            {
-                state[i]++;
-            }
-        }
+  pair<int, int> ans = {0, -1}; // {연주할 수 있는 곡의 수, 필요한 기타의 수}
+  for(int tmp = 0; tmp < (1 << n); tmp++){
+    long long comb = 0; // 조합한 결과
+    for(int i = 0; i < n; i++){
+      if((tmp & (1LL << i)) == 0)
+        continue;
+      comb |= state[i];
     }
-    check(0);
-    if(_min == 0x3fffffff) cout<<-1;
-    else cout<<_min;
+    int song_num = bit_cnt(comb);
+    int guitar_num = bit_cnt(tmp);
+    if(ans.first < song_num) // 1. 연주할 수 있는 곡의 수가 더 많을 경우
+      ans = {song_num, guitar_num};
+    // 2. 연주할 수 있는 곡의 수는 같은데 필요한 기타의 수가 더 적을 경우
+    else if(ans.first == song_num && ans.second > guitar_num)
+      ans = {song_num, guitar_num};
+  }
+  cout << ans.second << '\n';
 }
