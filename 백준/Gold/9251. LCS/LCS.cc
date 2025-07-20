6f957d449009aa8404c9815dfd6d02ef159f5ea6
강건우 <64396933+eric1306@users.noreply.github.com>
2025-07-20 13:11:32 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,22 +1,19 @@
-//Authored by: prid1306 // LCS
+// Authored by: prid1306
+// BOJ 9251
 #include <iostream>
 #include <string>
 using namespace std;
-string n,m;
 int d[1001][1001];
+string s1,s2;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>n>>m;
-    for(int i=0;i<n.size();i++) d[i][0] = 0;
-    for(int i=0;i<m.size();i++) d[0][i] = 0;
-    for(int i=1;i<=n.size();i++)
-    {
-        for(int j=1;j<=m.size();j++)
-        {
-            if(n[i-1] == m[j-1]) d[i][j] = d[i-1][j-1]+1;
-            else if(n[i-1]!=m[j-1]) d[i][j] = max(d[i-1][j],d[i][j-1]);
+    cin>>s1>>s2;
+    for(int i=1;i<=s1.size();i++){
+        for(int j=1;j<=s2.size();j++){
+            if(s1[i-1] == s2[j-1]) d[i][j] = d[i-1][j-1] + 1;
+            else d[i][j] = max(d[i-1][j],d[i][j-1]);
         }
     }
-    cout<<d[n.size()][m.size()];
+    cout<<d[s1.size()][s2.size()];
 }
