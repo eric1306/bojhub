6630fada9c065d6b4167a3a9eb6ca769591d40f3
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-03 23:25:25 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,10 @@
+// Authored by: prid1306
+// BOJ 
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout<<"406\n";
+    cout<<"eric1306\n";
+}
