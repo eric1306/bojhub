e5cc27efe759ef00e43e4fe1ef47e6ef174fa96e
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-07 14:45:22 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4564 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -0,0 +1,33 @@
+// Authored by: prid1306
+// BOJ 1520
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int n, m;
+int a[505][505], d[505][505];
+
+int func(int x, int y){
+    if(d[x][y] != -1) return d[x][y];
+    if(x == n - 1 && y == m - 1) return 1;
+    d[x][y] = 0;
+    for(int dir = 0; dir < 4; ++dir){
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if(a[x][y] > a[nx][ny]) d[x][y] += func(nx, ny);
+    }
+    return d[x][y];
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < m; ++j)
+            cin >> a[i][j], d[i][j] = -1;
+    cout << func(0, 0);
+}
