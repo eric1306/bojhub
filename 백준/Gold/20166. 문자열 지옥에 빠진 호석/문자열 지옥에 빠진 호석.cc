a6fcb45fd3eae6de6d55366395853744cb05e598
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-14 13:19:54 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 136 ms, Memory: 23944 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
@@ -7,20 +7,23 @@ using namespace std;
 int n,m,k;
 string a[11];
 //           좌  하 우 상 좌상 우상 우하 좌하
-int dx[8] = {-1, 0, 1, 0, -1, 1, 1, -1};
 int dy[8] = {0, 1, 0, -1, -1, -1, 1, 1};
+int dx[8] = {-1, 0, 1, 0, -1, 1, 1, -1};
 map<string, int> um;
 /* 
 이 풀이가 답이긴 한데, 최적화 하는법
 */
 void Solve(int sty, int stx, string s, int cnt){
-    if(cnt == 5) return;
     um[s]++;
+    if(cnt == 5) return;
     for(int i=0;i<8;i++){
-        int ny = (sty + dy[i] + n) % n;
-        int nx = (stx + dx[i] + m) % m;
-        string tmp = (s + a[ny][nx]);
-        Solve(ny, nx, tmp, cnt+1);
+        int ny = sty + dy[i];
+        int nx = stx + dx[i];
+        if(ny < 0) ny = n-1;
+        else if(ny == n) ny = 0;
+        if(nx < 0) nx = m-1;
+        else if(nx == m) nx = 0;
+        Solve(ny, nx, s + a[ny][nx], cnt+1);
     }
 }
 int main(){
