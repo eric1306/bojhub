faa17675f0298c34f5f07298f1b185031b42545b
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-20 02:17:33 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,24 +1,20 @@
-//Authored by: prid1306
+// Authored by: prid1306
 #include <iostream>
 #include <algorithm>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-int n;
-int arr[1001];
 int d[1001];
+int a[1001];
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n;
-    for(int i=1;i<=n;i++) cin>>arr[i];
-    fill(d,d+1001,1);
-    for(int i=2;i<=n;i++)
-    {
-        for(int j=i-1;j>=1;j--)
-        {
-            if(arr[i] > arr[j]){
-                d[i] = max(d[i],d[j] + 1);
-            }
+    FASTIO;
+    int n;cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    fill(d+1, d+n+1, 1);
+    for(int i=2;i<=n;i++){
+        for(int j=i-1; j>=1;j--){
+            if(a[j] < a[i])
+            d[i] = max(d[i], d[j]+ 1);
         }
     }
-    cout<<*max_element(d+1,d+1001);
+    cout<<*max_element(d+1, d+1001);
 }
