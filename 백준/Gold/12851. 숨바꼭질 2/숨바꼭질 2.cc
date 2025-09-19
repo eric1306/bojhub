6f24f396cfb5dbc624aa1eeee3af7090ac8471ea
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-19 17:20:06 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 8 ms, Memory: 4244 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,30 @@
+// Authored by: prid1306
+// BOJ 12851
+#include <iostream>
+#include <queue>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
+using namespace std;
+const int MAX = 200'002;
+int dist[MAX];
+int cnt[MAX];
+int main(){
+    FASTIO;
+    int n,k;cin>>n>>k;
+    fill(dist, dist+100002, -1);
+    queue<int> q;
+    q.push(n);
+    dist[n] = 0;
+    cnt[n] = 1;
+    while(!q.empty()){
+        int cur = q.front();q.pop();
+        for(int nxt : {cur - 1, cur + 1, cur*2})
+        {
+            if(nxt < 0 || nxt > 200'000) continue;
+            if(dist[nxt] != -1 && dist[cur] + 1 > dist[nxt]) continue;
+            if(dist[nxt] == -1) dist[nxt] = dist[cur] + 1;
+            cnt[nxt]++;
+            q.push(nxt);
+        }
+    }
+    cout<<dist[k]<<'\n'<<cnt[k];
+}
