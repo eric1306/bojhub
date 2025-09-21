48d836bed4fd383b8c1ef2005b1ae25a42a88986
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-21 15:32:18 +0900

    [Gold IV] Title: N-Queen, Time: 1712 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,34 +1,41 @@
+// Authored by: prid1306
 #include <iostream>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
+int n;
+int ans = 0;
+/*
+가로, 세로, 대각선을 고려해야한다
+가로 -> k행 마다 한개씩 배치하고 넘어가기 때문에 고려X
+세로 -> isused로 해당 열이 사용되었는지 체크하는 bool 배열 만들면 된다
+대각선 -> 대각선도 결국 배열로 표기 가능함
+ - 오른쪽 방향 대각선은 간단함(행 + 열 대각선 표기 가능.)
 
-int n,ans;
-int arr[16][16];
-int isused1[30];
-int isused2[30];
-int isused3[30];
-
-void func(int k){
+*/
+bool isusedcol[15];
+bool isusedright[30];
+bool isusedleft[30];
+void BT(int k)
+{
     if(k == n){
         ans++;
         return;
     }
-    for (int i = 0; i < n; i++) {
-        if(isused1[i] || isused2[i+k] || isused3[k-i+n-1])
+    for(int i=1;i<=n;i++){
+        if(isusedcol[i] || isusedright[i + k] || isusedleft[i + n - k])
             continue;
-        isused1[i] = 1;
-        isused2[i+k] = 1;
-        isused3[k-i+n-1] = 1;
-        func(k+1);
-        isused1[i] = 0;
-        isused2[i+k] = 0;
-        isused3[k-i+n-1] = 0;
+        isusedcol[i] = true;
+        isusedright[i + k] = true;
+        isusedleft[i + n - k] = true;
+        BT(k+1);
+        isusedcol[i] = false;
+        isusedright[i + k] = false;
+        isusedleft[i + n - k] = false;
     }
 }
-
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    FASTIO;
     cin>>n;
-    func(0);
+    BT(0);
     cout<<ans;
 }
