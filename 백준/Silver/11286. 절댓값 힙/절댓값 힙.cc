917714a5adf4a781508eba332ba5100572d85177
강건우 <64396933+eric1306@users.noreply.github.com>
2025-09-25 20:50:55 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -1,40 +1,32 @@
-//Authored by: prid1306
-#include <bits/stdc++.h>
+// Authored by: prid1306
 #include <iostream>
 #include <queue>
-#include <vector>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-
 class cmp{
 public:
-    bool operator() (int a, int b)
-    {
-        if(abs(a)!=abs(b)) return abs(a) > abs(b);
-        return a > 0 && b < 0;
+    bool operator()(int a, int b){
+        if(abs(a) > abs(b)) return true;
+        else if(abs(a) == abs(b)) return a > b;
+        else return false;
     }
 };
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    FASTIO;
     priority_queue<int, vector<int>, cmp> pq;
-    int n;
-    cin>>n;
-    while(n--)
-    {
-        int x;
-        cin>>x;
-        if(x == 0)
-        {
-            if(pq.empty()) cout<<"0\n";
-            else
-            {
+    int n;cin>>n;
+    while(n--){
+        int input;cin>>input;
+        if(input != 0){
+            pq.push(input);
+        }else{
+            if(pq.size() == 0){
+                cout<<0<<'\n';
+            }else{
                 cout<<pq.top()<<'\n';
                 pq.pop();
             }
         }
-        else 
-            pq.push(x);
     }
-    
-} 
+}
