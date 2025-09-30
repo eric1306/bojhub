37061bb4c7c4f79f4372482b1e0b64c0455da9f8
강건우 <64396933+eric1306@users.noreply.github.com>
2025-10-01 08:44:29 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 272 ms, Memory: 80144 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -5,21 +5,20 @@
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
 //수의 범위가 20'000'001 -> arr로 처리하기에는 너무 큰 범위이다.
-//char로 자료형은 선언한다고 해도 20'000'001 byte -> 20'000 KB -> 20MB, 되겠는데?
-
+//unsigned short 자료형은 선언한다고 해도 20'000'001 byte -> 20'000 KB -> 20MB, 되겠는데?
+const int AOFFSET = 10'000'000;
+int a[20'000'001];
 int main(){
     FASTIO;
     int n,m;cin>>n;
-    vector<int> v(n);
+    
     for(int i=0;i<n;i++){
-        cin>>v[i];
+        int input;cin>>input;
+        a[input + AOFFSET]++;
     }
-    sort(v.begin(), v.end());
     cin>>m;
     for(int i=0;i<m;i++){
         int req; cin>>req;
-        auto u = upper_bound(v.begin(), v.end(), req);
-        auto l = lower_bound(v.begin(), v.end(), req);
-        cout<<(u - l)<<' ';
+        cout<<a[req + AOFFSET]<<' ';
     }
 }
