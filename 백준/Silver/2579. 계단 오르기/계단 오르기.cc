1cfaa4e7294647048fefb1895a5da85b95433b92
강건우 <64396933+eric1306@users.noreply.github.com>
2025-10-21 18:28:27 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -3,7 +3,7 @@
 #include <algorithm>
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-int d[301][3];
+int d[301];
 int stairs[301];
 int main(){
     FASTIO;
@@ -11,15 +11,10 @@ int main(){
     for(int i=1;i<=n;i++){
         cin>>stairs[i];
     }
-    d[1][1] = stairs[1]; 
-    d[2][1] = stairs[2];d[2][2] = d[1][1] + stairs[2];
+    d[1] = stairs[1];
+    d[2] = stairs[1] + stairs[2];
     for(int i=3;i<=n;i++){
-        d[i][1] = max(d[i-2][1], d[i-2][2]) + stairs[i];
-        d[i][2] = d[i-1][1] + stairs[i];
+        d[i] = max(d[i-2], d[i-3] + stairs[i-1]) + stairs[i];
     }
-    int ans = -1;
-    for(int i=0;i<3;i++){
-        if(ans < d[n][i]) ans = d[n][i];
-    }
-    cout<<ans<<'\n';
+    cout<<d[n];
 }
