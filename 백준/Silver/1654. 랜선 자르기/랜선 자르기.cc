19c9b18d24e008b58c58b85804af96bb69ea8078
강건우 <64396933+eric1306@users.noreply.github.com>
2025-10-24 17:38:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -7,8 +7,7 @@ using ll = long long;
 int a[10001];
 int k,n;
 
-bool solve(int div)
-{
+bool solve(int div){
     ll ret = 0;
     for(auto elem : a)
         ret += elem/div;
@@ -17,14 +16,11 @@ bool solve(int div)
 
 int main(){
     FASTIO;
-    
-    cin>>k>>n;
-    for(int i=0;i<k;i++)
-        cin>>a[i];
+    cin >> k >> n;
+    for(int i=0;i<k;i++) cin>>a[i];
     ll st = 1;
     ll en = 0x7fffffff; //2^31 - 1 
-    while(st < en)
-    {
+    while(st < en){
         ll mid = (st + en + 1)/2;
         if(solve(mid)) st = mid;
         else en = mid - 1;
