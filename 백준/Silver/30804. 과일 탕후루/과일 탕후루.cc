d88402341a9b183ff9a81d18d3748553d681a400
강건우 <64396933+eric1306@users.noreply.github.com>
2025-10-28 14:40:02 +0900

    [Silver II] Title: 과일 탕후루, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -4,8 +4,8 @@
 #include <algorithm>
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-
-int CheckFruitType(const vector<int>& v)
+int n, ans;
+int GetNumberOfFuritTypes(const vector<int>& v)
 {
     int ret = 0;
     for(auto elem : v){
@@ -16,19 +16,17 @@ int CheckFruitType(const vector<int>& v)
 
 int main(){
     FASTIO;
-    int n,ans = 0;cin>>n;
+    cin>>n;
     vector<int> v(n);
     vector<int> c(10, 0);
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         cin>>v[i];
     }
     int l=0,r=0;
     while(r < n)
     {
         c[v[r++]]++;
-        while(CheckFruitType(c) > 2)
-            c[v[l++]]--;
+        while(GetNumberOfFuritTypes(c) > 2) c[v[l++]]--;
         ans = max(ans, r - l);
     }
     cout<<ans;
