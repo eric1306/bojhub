714d55f596d8371b27079dc131761dbd2d3ce47d
강건우 <64396933+eric1306@users.noreply.github.com>
2025-11-02 11:19:29 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,49 @@
+// Authored by: prid1306
+#include <iostream>
+#include <vector>
+#include <queue>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
+using namespace std;
+int n,m;
+
+int main(){
+    FASTIO;
+    cin>>n>>m;
+    vector<int> ladders(101, 0);
+    vector<int> snakes(101, 0);
+    for(int i=0;i<n;i++){
+        int st,en; cin>>st>>en;
+        ladders[st] = en;
+    }
+    for(int i=0;i<m;i++){
+        int st,en;cin>>st>>en;
+        snakes[st] = en;
+    }
+    vector<int> dist(101, 0);
+    queue<int> q;
+    q.push(1);
+    dist[1] = 0;
+    while(!q.empty())
+    {
+        int p = q.front();q.pop();
+        for(int i=1;i<7;++i)
+        {
+            int pos = p + i;
+            if(pos > 100) continue;
+            if(dist[pos]) continue;
+            dist[pos] = dist[p] + 1;
+            int tmp = pos;
+            while(ladders[pos]){
+                //dist[ladders[pos]] = dist[pos];
+                pos = ladders[pos];
+            }
+            while(snakes[pos]){
+                //dist[snakes[pos]] = dist[pos];
+                pos = snakes[pos];
+            }
+            if(dist[pos] == 0) dist[pos] = dist[tmp];
+            q.push(pos);
+        }
+    }
+    cout<<dist[100];
+}
