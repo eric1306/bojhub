16eec7a17030964a266d0eddc2180322a84f5d62
강건우 <64396933+eric1306@users.noreply.github.com>
2025-11-09 11:25:43 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -7,43 +7,38 @@
 using namespace std;
 #define y first
 #define x second
-int n,m,chickenCount, HouseCnt;
+int n,m,chickenCount;
 int ans = 1e9;
 using pii = pair<int,int>;
 int dy[4] = {1, 0, -1, 0};
 int dx[4] = {0, 1, 0, -1};
 int board[51][51];
 pii ChickenPos[14];
-int dist[51][51];
+vector<int> HouseDist;
 vector<pii> HousePos;
-//최대 m개니까, m개보다 작은 치킨집을 골랐을 때 치킨거리가 작게 되는 경우? -> 없지 않을까? 모르겠다.
 
 void UpdateDistFromChickenPos(int idx)
 {
-    for(auto house : HousePos)
+    for(int i=0;i<HousePos.size(); i++)
     {
-        int value = abs(house.y - ChickenPos[idx].y) + abs(house.x - ChickenPos[idx].x);
-        dist[house.y][house.x] = min(dist[house.y][house.x], value);
+        int value = abs(HousePos[i].y - ChickenPos[idx].y) + abs(HousePos[i].x - ChickenPos[idx].x);
+        HouseDist[i] = min(HouseDist[i] , value);
     }
 }
 
 int GetAllDist()
 {
     int ret = 0;
-    for(int i=0;i<51;i++) for(int j=0;j<51;j++){
-        if(dist[i][j] == 1e9) continue;
-        ret += dist[i][j];
-        //cout<<"dist["<<i<<"]["<<j<<"]: "<<dist[i][j]<<'\n';
+    for(auto elem : HouseDist){
+        if(elem == 1e9) continue;
+        ret += elem;
     }
-    // cout<<"ret: "<<ret<<'\n';
-    // cout<<"-------------\n";
     return ret;
 }
 
 int main(){
     FASTIO;
     cin>>n>>m;
-    //각 board 판의 info, 치킨집 개수와 position을 캐싱
     for(int i=0;i<n;i++) for(int j=0;j<n;j++){
         cin>>board[i][j];
         if(board[i][j] == 2)
@@ -52,31 +47,21 @@ int main(){
             HousePos.emplace_back(i,j);
     }
     vector<int> v(chickenCount, 0);
+    HouseDist.resize(HousePos.size(), 1e9);
     int size = static_cast<int>(v.size());
     for(int i= size - 1; i>size - 1 - m; i--){
         v[i] = 1;
     }    
     do{
-        // for(auto elem : v) 
-        //     cout<<elem<<' ';
-        // cout<<'\n';
-        for(int i=0;i<51;i++){
-            fill(dist[i], dist[i] + 51, 1e9);
-        }
-        //각 치킨집에서 집 번호까지 len 구하기
+        fill(HouseDist.begin(), HouseDist.end(), 1e9);
         for(int i=0;i<v.size(); ++i)
         {
-            if(v[i] == 1)
-            {
-                //cout<<"checked pos: ["<<i<<"] "<<ChickenPos[i].y<<' '<<ChickenPos[i].x<<'\n';
-                //그 치킨집이 체크해야할 치킨집이라면, 해당 치킨집으로부터 모든 집까지의 거리 구하기
+            if(v[i] == 1){
                 UpdateDistFromChickenPos(i);
             }
         }
-        //구해진 모든 집의 최소값과 ans를 비교해서 갱신
         int tmp = GetAllDist();
         ans = min(tmp, ans);
-        //cout<<"*********************\n";
     }while(next_permutation(v.begin(), v.end()));
     cout<<ans;
 }
