c4bcb621f274fa8675add297795cd35ce8c6ee76
강건우 <64396933+eric1306@users.noreply.github.com>
2025-11-26 18:17:55 +0900

    [Gold III] Title: 아기 상어, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -11,41 +11,36 @@ struct Pos{
 };
 int n,ans;
 int a[21][21];
+int dist[21][21];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 Pos BSPos;
 int BSLevel = 2;
 int EatFishAmount = 0;
 
-bool CanHunt(){
-    for(int i=0;i<n;i++) for(int j=0;j<n;j++){
-        if(a[i][j] < BSLevel) return true;
+void BFS(){
+    for(int i=0;i<21;i++){
+        fill(dist[i], dist[i] + 21, -1);
     }
-    return false;
-}
-
-int CanGo(Pos pos){
-    queue<pair<Pos, int>> q; //position, dist
+    queue<Pos> q; //position, dist
     bool visit[21][21];
     for(int i=0;i<n;i++) for(int j=0;j<n;j++) { visit[i][j] = false;}
     visit[BSPos.x][BSPos.y] = true;
-    q.push({BSPos, 0});
+    q.push(BSPos);
+    dist[BSPos.x][BSPos.y] = 0;
     while(!q.empty())
     {
-        auto [_pos, dist] = q.front(); q.pop();
+        auto p = q.front(); q.pop();
         for(int i=0;i<4;i++){
-            int nx = _pos.x + dx[i];
-            int ny = _pos.y + dy[i];
+            int nx = p.x + dx[i];
+            int ny = p.y + dy[i];
             if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
             if(visit[nx][ny] || a[nx][ny] > BSLevel) continue;
-            if(nx == pos.x && ny == pos.y){
-                return (dist + 1);
-            }
             visit[nx][ny] = true;
-            q.push({{nx, ny}, dist + 1});
+            dist[nx][ny] = dist[p.x][p.y] + 1;
+            q.push({nx, ny});
         }
     }
-    return 0;
 }
 
 class CMP
@@ -70,28 +65,28 @@ int main(){
     for(int i=0;i<n;i++) for(int j=0;j<n;j++)
     {
         cin>>a[i][j];
-        if(a[i][j] == 9) BSPos = {i, j};
+        if(a[i][j] == 9) 
+            BSPos = {i, j};
     }
 
     while(true) //사냥할 수 있는 먹이가 있을 때 400
     {
+        BFS();
         //먹이가 1개라면 이거 선택 
         //먹이가 2개 이상이라면 가장 거리가 짧은 먹이 중 가장 위, 왼쪽에 있는 걸 선택 400
         //가장 가까운 먹이 찾기
         Pos minPos = {};
         priority_queue<pair<Pos, int>, vector<pair<Pos, int>>, CMP> pq;
         for(int i=0;i<n;i++) for(int j=0;j<n;j++){
-            if(a[i][j] == 0 || a[i][j] == 9 || a[i][j] >= BSLevel) continue;
-            int dist = CanGo({i, j});
-            if(dist == 0) continue;
-            pq.push({{i, j}, dist});
+            if(a[i][j] == 0 || a[i][j] == 9 || a[i][j] >= BSLevel || dist[i][j] == -1) continue;
+            pq.push({{i, j}, dist[i][j]});
         }
         if(pq.empty()) break;
         minPos = pq.top().first;
         
         //cout<<"minPos : "<<minPos.x<<' '<<minPos.y<<'\n';
         //해당 먹이까지 갈 수 있는지 판단 400
-        int dist = CanGo(minPos);
+        int dist = pq.top().second;
         //cout<<"dist: "<<dist<<'\n';
         if(dist == 0) break;  //안된다면 break
         
