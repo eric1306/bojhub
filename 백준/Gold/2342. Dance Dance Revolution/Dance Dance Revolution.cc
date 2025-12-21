15d13c7180b88d32229a597a491a4398114b8754
강건우 <64396933+eric1306@users.noreply.github.com>
2025-12-21 13:31:20 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -1,13 +1,12 @@
 // Authored by: prid1306
 #include <iostream>
-#include <vector>
-#include <tuple>
+#include <algorithm>
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-vector<int> op;
 int l,r;
 int cost[4] = {1, 3, 4, 3};
-int dp[100'001][5][5];
+int DP[5][5];
+int newDP[5][5];
 /*
 dp[i][j][k]: i번째 명령까지 마쳤고, 플레이어가 (j, k)에 위치할 때의 value
 dp[i][j[k]
@@ -15,41 +14,27 @@ dp[i][j[k]
 
 int main(){
     FASTIO;
-    int input;
-    cin>>input;
-    for(int i=0;i<100'001;i++) for(int j=0;j<5;j++)
+    for(int j=0;j<5;j++)
     {
-        fill(dp[i][j], dp[i][j] + 5, 1e9);
+        fill(DP[j], DP[j] + 5, 1e9);
     }
-    while(input!=0)
+    DP[0][0] = 0;
+    while(true)
     {
-        op.emplace_back(input);
-        cin>>input;
-    }
-    dp[0][0][0] = 0;
-    for(int i=1;i<=op.size();i++)
-    {
+        int input;cin>>input;
+        if(input == 0) break;
+        for(int i=0;i<5;i++) fill(newDP[i], newDP[i] + 5, 1e9);
         for(int j=0;j<5;j++)
         {
             for(int k=0;k<5;k++)
             {
-                if(dp[i-1][j][k] != 1e9)
-                {
-                    //cout<<"i: "<<i-1<<" j: "<<j<<" k: "<<k<<" op: "<<op[i-1]<<'\n';
-                    dp[i][op[i-1]][k] = min(dp[i][op[i-1]][k], dp[i-1][j][k] + (j == 0 ? 2 : cost[abs(j - op[i-1])%4]));
-                    dp[i][j][op[i-1]] = min(dp[i][j][op[i-1]], dp[i-1][j][k] + (k == 0 ? 2 : cost[abs(k - op[i-1])%4]));
-                    //cout<<"dp["<<i<<"]["<<op[i-1]<<"]["<<k<<"]: "<<dp[i][op[i-1]][k]<<'\n';
-                    //cout<<"dp["<<i<<"]["<<j<<"]["<<op[i-1]<<"]: "<<dp[i][j][op[i-1]]<<'\n';
-                }
+                if(DP[j][k] == 1e9) continue;
+                newDP[input][k] = min(newDP[input][k], DP[j][k] + (j == 0 ? 2 : cost[abs(j - input)%4]));
+                newDP[j][input] = min(newDP[j][input], DP[j][k] + (k == 0 ? 2 : cost[abs(k - input)%4]));
             }
         }
+        swap(DP, newDP);
     }
-    int ans = 1e9;
-    int size = op.size();
-    for(int i=0;i<5;i++) for(int j=0;j<5;j++)
-    {
-        //cout<<dp[size][i][j]<<'\n';
-        ans = min(ans, dp[size][i][j]);
-    }
+    int ans = *min_element(&DP[0][0], &DP[0][0] + 25);
     cout<<ans;
 }
