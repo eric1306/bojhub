0c433ced97fe954a982c14a84fe6d1b65d6e97cd
강건우 <64396933+eric1306@users.noreply.github.com>
2026-01-04 13:45:27 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.cc
+++ 백준/Gold/10775. 공항/공항.cc
@@ -1,49 +1,35 @@
 // Authored by: prid1306
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
 #define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
 using namespace std;
-/*
-게이트 최대 100'000
-비행기 최대 100'000
-*/
+
+int g,p;
+int parent[100'001];
+int find(int x)
+{
+    if(parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
+}
+void uni(int u, int v)
+{
+    u = find(u);
+    v = find(v);
+    if(u!=v) parent[u] = v;
+}
 int main(){
     FASTIO;
-    int g,p;
     cin>>g>>p;
-    vector<int> v(p);
-    vector<bool> isfill(g, false);
-    map<int, int> m;
+    for(int i=0;i<=g;i++){
+        parent[i] = i;
+    }
+    int ans = 0; 
     for(int i=0;i<p;i++)
     {
-        cin>>v[i];
-    }
-    for(int i=0;i<g;i++)
-    {
-        m[i] = i;
-    }
-    int ans = 0;
-    for(int i=0;i<p;i++)
-    {
-        bool bflag = true;
-        int st = v[i] - 1;
-        for(int j=m[v[i] - 1]; j>= 0; j--)
-        {
-            if(!isfill[j])
-            {
-                bflag = false;
-                isfill[j] = true;
-                m[v[i] - 1] = j-1;
-                ans++;
-                break;
-            }
-        }
-        if(bflag)
-        {
-            break;
-        }
+        int gate;cin>>gate;
+        int root = find(gate);
+        if(root == 0) break;
+        uni(root, root - 1);
+        ans++;
     }
     cout<<ans;
 }
