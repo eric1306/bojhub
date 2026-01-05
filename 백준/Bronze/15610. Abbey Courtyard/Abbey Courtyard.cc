dd323787dd71b91175b42f04a5ccccbf64a7df5c
강건우 <64396933+eric1306@users.noreply.github.com>
2026-01-05 18:04:56 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,11 @@
+// Authored by: prid1306
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#define FASTIO cin.tie(0)->ios::sync_with_stdio(0)
+using namespace std;
+int main(){
+    long long line;
+    cin>>line;
+    cout<<setprecision(20)<<4*sqrt(line);
+}
